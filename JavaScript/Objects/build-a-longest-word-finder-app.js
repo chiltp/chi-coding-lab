1d5c4a8d7c2be1d492ca1891f1fde5da13596dfd
Luong Thi Phuong Chi <luongphuongchi1505@gmail.com>
2026-02-09 10:02:54 +0700

    Add longest-word finder function and tests
    Introduce findLongestWordLength in JavaScript: splits an input string on spaces and iterates to return the length of the longest word.

---
+++ JavaScript/Objects/build-a-longest-word-finder-app.js
@@ -0,0 +1,23 @@
+/*
+Create a function that takes a string as an argument and returns the length of the longest word in the string.
+If there are multiple words with the same maximum length, return the length of the first one encountered.
+*/
+
+function findLongestWordLength(str) {
+  const strSplit = str.split(" ");
+  let lengthOfLongestWord = strSplit[0].length;
+  for (let word of strSplit) {
+    if (word.length > lengthOfLongestWord) {
+       lengthOfLongestWord = word.length;
+    }
+  }
+  return lengthOfLongestWord;
+}
+
+// Test cases
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWordLength("May the force be with you"));
+console.log(findLongestWordLength("Google do a barrel roll"));
+console.log(findLongestWordLength("Googling do a barrel roll"));
+console.log(findLongestWordLength("What is the average airspeed velocity of an unladen swallow"));
+console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"));
