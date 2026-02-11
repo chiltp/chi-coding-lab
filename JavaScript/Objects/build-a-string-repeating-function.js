375e24a99330122726efec7ed75462339cdb4827
Luong Thi Phuong Chi <luongphuongchi1505@gmail.com>
2026-02-11 21:21:19 +0700

    Add repeatStringNumTimes function with tests
    Introduce repeatStringNumTimes(str, num) which returns str repeated num times and returns an empty string for non-positive num.

---
+++ JavaScript/Objects/build-a-string-repeating-function.js
@@ -0,0 +1,21 @@
+/*
+Create a function that repeats a given string (str) for num times.
+Return an empty string if num is not a positive number.
+*/
+function repeatStringNumTimes (str, num) {
+  let result = "";
+  if (num <= 0) {
+    return result;
+  }
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+  return result;
+}
+
+// Test cases
+console.log(repeatStringNumTimes("*", 3)); // "***"
+console.log(repeatStringNumTimes("abc", 3)); // "abcabcabc"
+console.log(repeatStringNumTimes("abc", 4)); // "abcabcabcabc"
+console.log(repeatStringNumTimes("abc", 1)); // "abc"
+console.log(repeatStringNumTimes("abc", -2)); // ""
